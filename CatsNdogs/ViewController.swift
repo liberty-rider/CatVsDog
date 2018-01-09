7151e002d919d1fcc885d0d951d583ff20c89a07
Mathieu Vandeginste <matvdg@me.com>
2018-01-09 10:31:08 +0100

    hello, world!

---
+++ CatsNdogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CatsNdogs
+//
+//  Created by Mathieu Vandeginste on 08/01/2018.
+//  Copyright © 2018 LibertyRider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
