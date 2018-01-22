cc94d0650a794c2277ccfcb10c999ceb3cf2b39b
Mathieu Vandeginste <matvdg@me.com>
2018-01-22 15:23:54 +0100

    refactor UIImage extension + predictor

--- CatsNdogs/Libs/UIImage+CVPixelBuffer.swift
+++ CatsNdogs/Libs/UIImage+CVPixelBuffer.swift
@@ -9,32 +9,31 @@
 import UIKit
 
 extension UIImage {
+    
+    func resize(toSize: CGSize) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(toSize, false, 0.0)
+        self.draw(in: CGRect(x: 0, y: 0, width: toSize.width, height: toSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
     func toCVPixelBuffer() -> CVPixelBuffer? {
         let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
         var pixelBuffer : CVPixelBuffer?
         let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(self.size.width), Int(self.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
-        guard status == kCVReturnSuccess else {
-            return nil
-        }
-        
-        if let pixelBuffer = pixelBuffer {
-            CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
-            let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
-            
-            let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-            let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
-            
-            context?.translateBy(x: 0, y: self.size.height)
-            context?.scaleBy(x: 1.0, y: -1.0)
-            
-            UIGraphicsPushContext(context!)
-            self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
-            UIGraphicsPopContext()
-            CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
-            
-            return pixelBuffer
-        }
-        
-        return nil
+        guard status == kCVReturnSuccess, let buffer = pixelBuffer else { return nil }
+        CVPixelBufferLockBaseAddress(buffer, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(buffer)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(self.size.width), height: Int(self.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(buffer), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue)
+        context?.translateBy(x: 0, y: self.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        UIGraphicsPushContext(context!)
+        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(buffer, CVPixelBufferLockFlags(rawValue: 0))
+        return buffer
     }
+    
 }
--- CatsNdogs/Predictor.swift
+++ CatsNdogs/Predictor.swift
@@ -18,19 +18,11 @@ class Predictor {
     private let model = CatAndDog()
     private let size = CGSize(width: 150, height: 150)
     
-    private func resize(image: UIImage, newSize: CGSize) -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
-        image.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return newImage
-    }
-    
     func predict(image: UIImage) -> Species? {
         do {
-            guard let resizedImage = self.resize(image: image, newSize: self.size), let pixelBuffer = resizedImage.toCVPixelBuffer() else { return nil }
+            guard let resizedImage = image.resize(toSize: self.size), let pixelBuffer = resizedImage.toCVPixelBuffer() else { return nil }
             let output = try self.model.prediction(image: pixelBuffer)
-            guard let prediction = output.output1[0] as? Int else { print("prediction is nil"); return nil }
+            guard let prediction = output.output1[0] as? Int else { return nil }
             return Species(rawValue: prediction)
         } catch {
             print("Error while doing predictions: \(error)")
